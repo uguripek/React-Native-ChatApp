39039fa871841ee2e1f31d4fc62d8c83f15ac270
hahaha <nguyensonha.hanz@gmail.com>
2020-02-12 00:48:56 +0700

    Render message using Flatlist

--- Chat-App/Screens/ChatScreen.js
+++ Chat-App/Screens/ChatScreen.js
@@ -1,35 +1,80 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, TextInput, Button, Text, FlatList } from 'react-native';
 import MessageBubble from '../components/MessageBubble';
+import { firebaseApp } from '../config/FirebaseConfig'
 
 class ChatScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      message: '',
+      name: '',
+      icon: '',
+      bubbleColors: '',
+      chatData: [],
     };
+  }
+
+  async componentDidMount() {
+    const userId = firebaseApp.auth().currentUser.uid
+    console.log(userId);
+    
+    //this.setState({name})
+    firebaseApp.database().ref('Users/' + userId).on("value", snapshot => {
+      this.setState({ 
+        name: snapshot.val().Name,
+        icon: snapshot.val().Icon,
+        bubbleColor: snapshot.val().Color,
+      })      
+    })
+    firebaseApp.database().ref('/Messages').on("value", snapshot => {
+      if(snapshot.val() !== undefined && snapshot.val() !== null )
+      {
+          this.setState({
+              chatData: Object.values(snapshot.val())
+          });
+      }
+      
+  });
+  }
+
+  _renderChatLine = (item) =>
+    {
+        return(
+            <MessageBubble icon={item.Icon} name={item.Name}  message={item.Message} bubbleColor={item.Color} />
+        );
+    };
+
+  send() {
+    const userName = this.state.name
+    firebaseApp.database().ref('Messages/').push({
+      Name: userName,
+      Message: this.state.message,
+      Icon: this.state.icon,
+      Color: this.state.bubbleColor
+    })
+    this.setState({ message: '' })
   }
 
   render() {
     return (
-      <View>
-        <MessageBubble
-          name='꧁༒•TheKing•༒꧂ '
-          icon='👅'
-          message='askdjhaskdajh'
-          bubbleColor='#f9ff53'
-        />
-        <MessageBubble
-          name='꧁༒•TheKing•༒꧂ '
-          icon='👅'
-          message='askdjhaskdajh'
-          bubbleColor='#ff3d67'
-        />
-        <MessageBubble
-          name='꧁༒•TheKing•༒꧂ '
-          icon='👅'
-          message='askdjhaskdajh'
-          bubbleColor='#e82e7f'
-        />
+      <View style={{ flex: 1}}>
+        <View style={{ flex: 1 }}>
+        <FlatList data={this.state.chatData} renderItem={({item}) => this._renderChatLine(item)} />
+        </View>
+        <View style={{ flex: 1 }}>
+          <TextInput
+            placeholder="inputhere"
+            onChangeText={(message) => this.setState({message})}
+            value={this.state.message}
+            style={{ borderColor: 'black', borderWidth: 1 }}
+          />
+          <Button
+            title='gui'
+            style={{ height: 30, width: 200 }}
+            onPress={() => this.send()}
+          />
+        </View>
       </View>
     );
   }
