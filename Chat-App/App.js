1d8267edc0f39dfd9a91851c008e505a52f57e13
hahaha <nguyensonha.hanz@gmail.com>
2020-02-11 19:36:55 +0700

    Edit App.js

--- Chat-App/App.js
+++ Chat-App/App.js
@@ -3,17 +3,6 @@ import LoginScreen from './Screens/LoginScreen'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <LoginScreen/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
